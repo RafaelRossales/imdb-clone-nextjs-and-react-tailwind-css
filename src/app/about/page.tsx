0d3a329436a14f0918f1c01b4c53d4b6cee357de
RafaelRossales <rapha2704@gmail.com>
2024-07-01 22:22:23 -0300

    Implementing About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 text-justify'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec est aliquam, vulputate turpis pretium, dignissim orci. Nunc lectus ex, semper a pellentesque nec, egestas vel lorem. Etiam tincidunt suscipit porttitor. Ut pharetra eget eros id molestie. Nulla aliquet aliquet pulvinar. In mollis tellus lorem, sollicitudin mollis nibh mollis tristique. Aliquam sit amet nisi lectus. Sed in nulla ultricies eros tincidunt pharetra sit amet mollis turpis. Duis sit amet diam ornare, vestibulum tellus non, viverra mi. Donec vel tempus odio. Donec congue diam dui. Mauris eget felis sed augue efficitur tincidunt. Praesent placerat, ex a efficitur placerat, arcu enim tempus augue, ut tincidunt dui massa et massa. Nulla bibendum ex id lectus elementum, a fringilla augue bibendum.
+      </p>
+      <p>
+      Aliquam eu metus ut mauris auctor ultrices. Pellentesque volutpat, metus ut interdum tempus, sapien mi iaculis felis, id scelerisque magna elit vitae quam. Nulla in volutpat odio. Sed mi urna, fringilla id sem sed, pulvinar tempor ex. Nulla dapibus non orci vehicula feugiat. Suspendisse massa nunc, placerat mattis nisl ut, pharetra facilisis felis. Integer vulputate nibh in magna auctor, a tempus ligula dapibus. Maecenas pharetra a erat id aliquet. Cras ante libero, facilisis in euismod a, sollicitudin nec ipsum. Suspendisse eget eros a turpis blandit viverra. Nulla accumsan nibh arcu, ut eleifend justo venenatis at. Sed ac risus iaculis, pharetra odio et, tempus tellus. Aliquam sit amet consequat metus. Nam rutrum sed leo a euismod. Proin eu ex mattis, imperdiet sapien ut, mollis enim.
+      </p>
+      <p>
+      Praesent sed feugiat dui. Fusce vitae velit a mi vehicula elementum. Morbi id nibh ut metus mollis condimentum vel eu risus. Curabitur auctor vel justo non tempor. Quisque volutpat pretium vulputate. Aliquam cursus, tellus pulvinar ornare mollis, enim dui aliquam est, quis consectetur libero dui eu enim. Curabitur auctor varius tortor vel tristique. Cras mattis quam non ligula elementum, eu tempus leo dapibus. Suspendisse ultricies libero non commodo placerat. In vitae vestibulum massa.
+      </p>
+    </div>
+  )
+}
